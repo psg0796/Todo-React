7e091eef47b3b0b6d4480d1ca58e3a5bbc920ce6
psg0796 <guptaprashantshekhar@gmail.com>
2018-12-04 18:48:37 +0530

    ToDo and Completed: 	implemented:- 		ToDo part with dummy values.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+function Item(props){
+  return (
+      <div>
+        <button onClick={props.onClick}>
+          {props.value}
+        </button>
+        <button onClick={props.onClick}>
+          X
+        </button>
+        
+      </div>
+    );
+}
+
+class List extends Component {
+  renderItem(i) {
+    return <Item value={this.props.items[i]}
+    onClick={()=>this.props.onClick(i)}/>;
+  }
+
   render() {
+    let i = 0;
+    const listItems = this.props.items.map((item,index)=>
+        <li key={index}>
+          {this.renderItem(i)}
+          {i++}
+        </li>
+      );
     return (
+        <ul>
+          {listItems}
+        </ul>
+      )
+  }
+}
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      items : Array(0),
+    }
+  }
+
+  add(){
+    this.setState(state => ({
+      items: state.items.concat(['new Item'])
+    }))
+  }
+
+  handleClick(index){
+    const items = this.state.items;
+    items.splice(index,1);
+    this.setState({items});
+  }
+
+  render() {
+      return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <div>Todo</div>
+        <List items={this.state.items} onClick={(i)=>this.handleClick(i)}/>
+        <button onClick={()=> this.add()}>+</button>
         </header>
       </div>
     );
